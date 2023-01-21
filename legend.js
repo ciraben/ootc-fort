0b78e9e8d3219c86ef04ca4e0cf7a98c3b833516
ciraben <tom.on.github@gmail.com>
2023-01-21 00:19:14 -0800

    Add civilizations overlay.

--- legend.js
+++ legend.js
@@ -1,8 +1,8 @@
 const geooverlay = document.querySelector("#georegion");
 const geooverlayToggle = document.querySelector("#geo-overlay-toggle");
-/*
 const civoverlay = document.querySelector("#civilizations");
 const civoverlayToggle = document.querySelector("#civ-overlay-toggle");
+/*
 const eventsoverlay = document.querySelector("#events");
 const eventsoverlayToggle = document.querySelector("#event-overlay-toggle");
 */
@@ -14,7 +14,6 @@ geooverlayToggle.addEventListener("change", function() {
     geooverlay.style.display = "none";
   }
 });
-/*
 civoverlayToggle.addEventListener("change", function() {
   if (this.checked) {
     civoverlay.style.display = "block";
@@ -22,6 +21,7 @@ civoverlayToggle.addEventListener("change", function() {
     civoverlay.style.display = "none";
   }
 });
+/*
 eventsoverlayToggle.addEventListener("change", function() {
   if (this.checked) {
     eventsoverlay.style.display = "block";
