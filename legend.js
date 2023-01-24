f4672ad42f8754450ff06f66b81c658adf7560d7
ciraben <tom.on.github@gmail.com>
2023-01-23 17:21:39 -0800

    Add Journey/Events overlay.

--- legend.js
+++ legend.js
@@ -2,10 +2,8 @@ const geooverlay = document.querySelector("#georegion");
 const geooverlayToggle = document.querySelector("#geo-overlay-toggle");
 const civoverlay = document.querySelector("#civilizations");
 const civoverlayToggle = document.querySelector("#civ-overlay-toggle");
-/*
 const eventsoverlay = document.querySelector("#events");
 const eventsoverlayToggle = document.querySelector("#event-overlay-toggle");
-*/
 
 geooverlayToggle.addEventListener("change", function() {
   if (this.checked) {
@@ -21,7 +19,6 @@ civoverlayToggle.addEventListener("change", function() {
     civoverlay.style.display = "none";
   }
 });
-/*
 eventsoverlayToggle.addEventListener("change", function() {
   if (this.checked) {
     eventsoverlay.style.display = "block";
@@ -29,4 +26,3 @@ eventsoverlayToggle.addEventListener("change", function() {
     eventsoverlay.style.display = "none";
   }
 });
-*/
